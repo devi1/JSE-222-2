918246a99940cf954f1f124544fb44de2fe9a554
Damir <damiryessenbek@gmail.com>
2023-01-25 16:11:12 +0600

    Введение в программирование

---
+++ HTML CSS/2023.01.13/script.js
@@ -0,0 +1,10 @@
+let num = 16;
+let str = "wertyuioldjhds";
+let bool = false; // true
+let n;
+let o = null;
+let i = NaN;
+
+let s = num+str+bool+n+o+i;
+console.log(typeof n);
+console.log(s);
---
+++ JS/2023.01.25/script.js
@@ -0,0 +1,44 @@
+// let number;
+// console.log(number);
+
+// let number = +prompt("Введи любое число");
+// console.log(typeof number);
+// console.log(number);
+// let numberOfTimes = number * 5;
+// let s = 2;
+// console.log(s);
+// number = s;
+// number--;
+// console.log(number);
+// alert(number);
+// let check = confirm("Are you sure you want to");
+// console.log(check);
+// number числа
+// string строки
+// boolean true | false  булеан истина/ложь
+// undefined неизвестная переменная
+// null нулл
+// object объект
+
+// +, -, *, /, %
+
+// ++s; a++
+// --a; a--
+// = == ===
+
+let a = 2;
+let b = "2";
+
+if(a === b){
+    console.log("They are match!");
+}
+else if (a > b){
+    console.log("a more than b!");
+}
+else{
+    console.log("a less than b!");
+}
+
+число**степень
+
+Math.pow(число, степень)
