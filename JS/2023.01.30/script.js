273bea2e378b750b871c4375c110783f9ff7fd27
Damir <damiryessenbek@gmail.com>
2023-01-30 20:44:03 +0600

    Циклы

--- JS/2023.01.30/script.js
+++ JS/2023.01.30/script.js
@@ -41,15 +41,78 @@
 
 //// Task 3
 
-let a = +prompt("Enter the number: "); // 4
-let b = +prompt("Enter the power: "); // 5
-let c = a;
-let d = 1;
-while(d != b){ // 1
-    a *= c; // 1024*4
-    d++; // 4-1=3
-    console.log("Power: "+d);
-    console.log("Number: "+a);
-}
+// let a = +prompt("Enter the number: "); // 4
+// let b = +prompt("Enter the power: "); // 5
+// let c = a;
+// let d = 1;
+// while(d != b){ // 1
+//     a *= c; // 1024*4
+//     d++; // 4-1=3
+//     console.log("Power: "+d);
+//     console.log("Number: "+a);
+// }
 
-console.log("Result: "+a); //
+// console.log("Result: "+a); 
+
+//// Task 1
+
+// let a;
+// do{
+//     a = +prompt("Your answer is: ");
+// }while(a != (2+2*2))
+
+// alert("Result: "+a);
+
+//// Task 2
+
+// let a = 1000;
+// let cnt = 0;
+
+// do{
+//     a=a/2
+//     console.log(a);
+//     cnt++;
+// }while(a > 50);
+
+// alert("Result: "+a+"\n" + "Count: "+cnt);
+
+//// Task 3
+
+// let a = +prompt("Enter the number:");
+
+// for(let i = a; i <= 100; i++){
+//     if(i%a==0){
+//         alert(i);
+//     }
+// }
+
+//// Task 4
+
+// let a = +prompt("Enter the first number: ");
+// let b = +prompt("Enter the second number: ");
+// let c = 1;
+// for(let i = a; i <= b; i++){
+//     if(c%4==0){
+//         console.log(i);
+//         c++;
+//     }
+//     c++;
+// }
+
+//// Task 5
+
+let a = +prompt("Enter the number: ");
+let cnt = 0;
+
+for(let i = 2; i < a; i++){
+    console.log(i);
+    if(a%i == 0){
+        cnt++;
+    }
+}
+if(cnt == 0){
+    alert("Number is prime");
+}
+else{
+    alert("Number is not prime");
+}
