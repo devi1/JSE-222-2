40c33784e999e7ea1164bca71da9a6efaa0c65a0
Damir <damiryessenbek@gmail.com>
2023-02-01 14:11:15 +0600

    Функции

---
+++ JS/2023.02.01/script.js
@@ -0,0 +1,44 @@
+// function factorial(num){
+//     // return n?n*factorial(n-1):1;
+//     if(num){
+//         return num*factorial(num-1);
+//     }
+//     else{
+//         return 1;
+//     }
+// }
+// let num = +prompt("Enter the number")
+// console.log(factorial(num));
+
+// if(n){
+//     return n*factorial(n-1);
+// }
+// else{
+//     return 1;
+// }
+
+// function mathR(num){
+//     return num*num+num-num/num;
+// }
+
+// let a = +prompt("ENter the number")
+// let b = +prompt("ENter the number")
+// let c = +prompt("ENter the number")
+// let d = +prompt("ENter the number")
+// let e = +prompt("ENter the number")
+// let f = +prompt("ENter the number")
+// let g = +prompt("ENter the number")
+// let h = +prompt("ENter the number")
+// let i = +prompt("ENter the number")
+// let j = +prompt("ENter the number")
+
+// alert(mathR(a))
+// alert(mathR(b))
+// alert(mathR(c))
+// alert(mathR(d))
+// alert(mathR(e))
+// alert(mathR(f))
+// alert(mathR(g))
+// alert(mathR(h))
+// alert(mathR(i))
+// alert(mathR(j))
