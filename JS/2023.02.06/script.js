007c52c5f08e5cf1a4c006150e4e5d32c5ec5fc8
Damir <damiryessenbek@gmail.com>
2023-02-06 20:19:27 +0600

    Массивы

---
+++ JS/2023.02.06/script.js
@@ -0,0 +1,47 @@
+// let arr = [1,3,6,7,2,4,5,-1, 7,5];
+// console.log(arr);
+// delete arr[2];
+// arr.sort();
+// console.log(arr);
+
+// arr.splice(2, 2);
+// arr.splice(-1, 0, 6, 7, 8);
+
+// let newArr = [7,8,9,10];
+// let newArr2 = [11,12];
+
+// console.log(arr.concat(newArr, newArr2, newArr));
+
+// arr.forEach(function(item, index, arr){
+//     console.log(`${item} has ${index} index in ${arr}`);
+// })
+
+// function arrSort(a, b){
+//     if(a>b) return 1;
+//     if(a<b) return -1;
+//     if(a==b) return 0;
+// }
+
+// arr.sort(arrSort);
+
+// console.log(arr);
+
+// arr.reverse();
+
+// console.log(arr);
+
+
+// let str = 'qwe,rtr,asd,gfl,asd,gr'
+
+// let arr = str.split(',');
+
+// console.log(arr);
+
+// str = arr.join('GLUE');
+// console.log(str);
+
+let arr = [1,2,3,4,5,6];
+
+arr.fill(9, 1,4)
+
+console.log(arr);
