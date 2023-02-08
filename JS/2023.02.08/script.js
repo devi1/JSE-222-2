c3950ebab8a3439c107f8d5985415a25c5d037d3
Damir <damiryessenbek@gmail.com>
2023-02-08 15:58:14 +0600

    Revise

--- JS/2023.02.08/script.js
+++ JS/2023.02.08/script.js
@@ -25,3 +25,34 @@
 // break; // завершить цикл
 // continue; // пропустить одну итерацию
 
+
+
+//// Task 10
+
+// let N = +prompt("Your number?");
+
+// let N = Math.floor(Math.random()*101)
+
+// while(true){
+//     let guess = +prompt("Guess the number: ");
+
+// }
+
+let guessedNum = Number(alert("Загадайте число от 1 до 100")); // Создается переменная числа которое мы загадали.
+let lowerLimit = 0; // Нижний лимит
+let upperLimit = 100; // Верний лимит
+let N = 50; // Само предпологаемое число по мнению компьютера
+
+while(guessedNum !== N) { // создается цикл с условием пока не станет равно
+    let answer = prompt(`Ваше число >, <, или равно ${N}`, '>, <, ='); // Запрашиваем у польщователя  предположение компьютера
+    if (answer == '>') {
+        lowerLimit = N; // Меняем лимит на число N
+        N = Math.ceil((upperLimit - lowerLimit) / 2 + lowerLimit); // Меняем само число N
+    } else if (answer == '<') {
+        upperLimit = N; // Меняем лимит на число N
+        N = Math.ceil((upperLimit - lowerLimit) / 2 + lowerLimit); // МЕняем число N
+    } else {
+        guessedNum = N; // 
+    };
+};
+alert(`Задуманное число ${N}`);
