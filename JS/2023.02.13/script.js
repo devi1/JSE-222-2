ab2cf8313b9b25c8f286ee93f79f8457d03d3da0
Damir <damiryessenbek@gmail.com>
2023-02-13 20:04:10 +0600

    Строки

---
+++ JS/2023.02.13/script.js
@@ -0,0 +1,64 @@
+// function functionName(a, b, c){
+//     alert("Hello " + a + ", " + b + ", " + c);
+// }
+// let q = "damir";
+// let w = "IT-Step";
+// let e = "aswertyuikl;kmhjbgtr";
+
+// let name1 = "Damir";
+// let name2 = "Assel";
+// let name3 = "Oleg";
+// let name4 = "Petr";
+// let name5 = "Dina";
+// let name6 = "Anar";
+// let name7 = "Sanzhar";
+// let name8 = "Timur";
+
+// function sayHello(name){
+//     alert(`Welcome to the lesson ${name}! We are glad to see you here.`)
+// }
+
+// sayHello(name1);
+// sayHello(name2);
+// sayHello(name3);
+// sayHello(name4);
+// sayHello(name5);
+// sayHello(name6);
+// sayHello(name7);
+// sayHello(name8);
+
+// let string1 = 'single-quoted';
+// let string2 = "double-quoted";
+// let string3 = `backtick`;
+
+// console.log(string3);
+
+// alert(`This is ${string1} string`)
+
+let str = "qwertyuiopplkjhgfdsaZxcvbnm,";
+
+// console.log(str.length);
+// console.log(str.at(-3));
+
+// for(let c of str){
+//     console.log(c);
+// }
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str.indexOf('Zxcv'));
+
+// let target = "p";
+
+// let pos = 0;
+
+// while(true){
+//     let fPos = str.indexOf(target, pos);
+//     if(fPos == -1) break;
+//     console.log(fPos);
+//     pos = fPos + 1;
+// }
+
+// console.log(str.substring(2, 6));
+// console.log(str.substring(6));
