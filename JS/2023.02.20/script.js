5bf34e3d67154b4a362e4ab2e6a97c8dd9c9db49
Damir <damiryessenbek@gmail.com>
2023-02-21 20:05:59 +0600

    Revise

--- JS/2023.02.20/script.js
+++ JS/2023.02.20/script.js
@@ -1,12 +1,36 @@
-function stringOdd(){
-    let inputValue = document.getElementsByTagName('input')[0].value;
-    let arr = inputValue.split('');
-    console.log(arr);
-    for(let i = 0; i < arr.length; i++) {
-        if(i%2==0){
-            arr.splice(i, 1, 1);
-            // arr[i] = 1;
-        }
-    }
-    console.log(arr);
+function changeColor(){
+    let r = Math.round(Math.random()*256);
+    let g = Math.round(Math.random()*256);
+    let b = Math.round(Math.random()*256);
+    console.log(r);
+    console.log(g);
+    console.log(b);
+    // let div = document.getElementById('myDiv');
+    let newDiv = document.createElement("div");
+    document.body.append(newDiv)
+    newDiv.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    // div.append(div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`);
+    onDiv();
 }
+
+// let body = document.querySelector('body');
+
+// function dicso(){
+//     let r = Math.round(Math.random()*256);
+//     let g = Math.round(Math.random()*256);
+//     let b = Math.round(Math.random()*256);
+
+//     document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+// }
+
+// body.addEventListener('mousemove', dicso)
+
+// function onDiv(){
+    let div = document.getElementsByTagName('div')[0];
+    div.addEventListener('mousemove', function(){
+        let r = Math.round(Math.random()*256);
+        let g = Math.round(Math.random()*256);
+        let b = Math.round(Math.random()*256);
+        div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    });
+// }
