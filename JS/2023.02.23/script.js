d168c624df806b7c56da847bb250d69f94b1fe76
Damir <damiryessenbek@gmail.com>
2023-02-23 19:48:06 +0600

    Встроенные объекты

---
+++ JS/2023.02.23/script.js
@@ -0,0 +1,22 @@
+// let dateNow = new Date();
+// console.log(dateNow);
+
+// let millDate = new Date(1000*3600*24);
+// console.log(millDate);
+
+// let customDate = new Date(2023, 1, 23, 19,30,51);
+// console.log(customDate);
+
+// console.log(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate(), dateNow.getHours(), dateNow.getMinutes(), dateNow.getDay());
+
+let now = new Date();
+console.log(now);
+now.setFullYear(2008);
+now.setMonth(5);
+now.setDate(32);
+
+console.log(now);
+
+console.log(Math.E)
+
+console.log(Math.sqrt(81));
