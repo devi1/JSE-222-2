9fc501495d49b851239b3b53df26bf886c9b72f3
Damir <damiryessenbek@gmail.com>
2023-03-03 19:10:56 +0600

    Cookie

---
+++ JS/2023.03.01/script.js
@@ -0,0 +1,13 @@
+// let myName = "my name";
+// let value = "Damir Yessenbek"
+
+// document.cookie = encodeURIComponent(myName) + "=" + encodeURIComponent(value);
+
+// document.cookie = "user=Damir; expires=Thu, 01 Mar 2023 21:08:00 GMT";
+
+document.cookie = "user=Damir"
+
+alert(document.cookie);
+
+
+alert(document.cookie);
