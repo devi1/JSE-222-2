c8ce31eca611159d426c8af7b25546b416224eb6
Damir <damiryessenbek@gmail.com>
2023-03-03 19:34:56 +0600

    Local Storage

---
+++ JS/2023.03.03/script.js
@@ -0,0 +1,14 @@
+localStorage.setItem('user', 'Damir');
+localStorage.setItem('year', 2023);
+
+console.log(localStorage.getItem('user'));
+console.log(localStorage.getItem('year'));
+
+
+// localStorage.removeItem('year');
+
+console.log(localStorage.length);
+
+console.log(localStorage.key(1));
+
+localStorage.clear();
