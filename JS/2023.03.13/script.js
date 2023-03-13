f2776792728db60b5c97be47c6debbff3afbc5d8
Damir <damiryessenbek@gmail.com>
2023-03-13 20:01:27 +0600

    RegExp

---
+++ JS/2023.03.13/script.js
@@ -0,0 +1,23 @@
+// let regexp = new RegExp("шаблон", "флаги")
+
+// let regexp = /шаблон/;
+// let regexp = /\D\W\S/gmi;
+
+// \d - digits 
+// \w - words 
+// \s - space 
+
+// \D - NOT digit
+// \W - NOT word
+// \S - NOT space
+
+let str = "HTML5";
+let str2 = "HT4L5";
+let str3 = "HT L5";
+let str4 = "HT-L5";
+let regexp = /z{5}c{2,}/gi;
+
+console.log(str.match(regexp)); 
+console.log(str2.match(regexp)); 
+console.log(str3.match(regexp)); 
+console.log(str4.match(regexp)); 
