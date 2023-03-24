a0388fc9a8482b75ced30b335ff8dcd9e022c897
Damir <damiryessenbek@gmail.com>
2023-03-24 20:06:48 +0600

    AJAX

--- JS/2023.03.17/script.js
+++ JS/2023.03.17/script.js
@@ -1,23 +1,23 @@
-// function myFunc(a, b){
-//     return a+b;
-// }
+function myFunc(a, b){
+    return a+b;
+}
 
-// let btn = document.getElementById("button");
-// btn.onclick = function(){
-//     console.log(this)
-// }
+let btn = document.getElementById("button");
+btn.onclick = function(){
+    console.log(this)
+}
 
-// btn.onclick = () =>{
-//     console.log(this)
-// }
+btn.onclick = () =>{
+    console.log(this)
+}
 
-// let sum = () => "sum";
+let sum = () => "sum";
 
-// console.log(sum())
+console.log(sum())
 
-// function sum(){
-//     return "sum"
-// }
+function sum(){
+    return "sum"
+}
 
 function task(q, a1, a2){
     if(confirm("Can you ask the question?")) return true;
@@ -28,5 +28,17 @@ task(
     "Do you agree?",
     function(){console.log("You answered yes")},
     function(){console.log("You answered no")}
+)
+
+
+function task(q, a1, a2){
+    if(confirm("Can you ask the question?")) return true;
+    else return false;
+}
+
+task(
+    "Do you agree?",
+    () => {("You answered yes")},
+    () => {("You answered no")}
 )
 
