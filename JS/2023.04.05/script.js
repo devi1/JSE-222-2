2604902bc23dcb820ec0586037f938570f872155
Sugar <damiryessenbek@gmail.com>
2023-04-05 20:22:40 +0600

    jQuery

---
+++ JS/2023.04.05/script.js
@@ -0,0 +1,74 @@
+// Hide
+
+// $(document).ready(function() {
+//     $("#hide").click(function() {
+//         $('div').hide(1000);
+//     })
+//     $("#show").click(function() {
+//         $('div').show('fast');
+//     })
+// })
+
+
+// Toggle
+
+// $(document).ready(function() {
+//     $("button").click(function() {
+//         $('div').toggle(100 );
+//     })
+// })
+
+// FadeIn
+
+// $(document).ready(function() {
+//     $("button").click(function() {
+//         $('div').fadeIn();
+//     })
+// })
+
+// FadeOut
+
+// $(document).ready(function() {
+//     $("button").click(function() {
+//         $('div').fadeOut();
+//     })
+// })
+
+// Fade Toggle
+
+// $(document).ready(function() {
+//     $("button").click(function() {
+//         $('div').fadeToggle();
+//     })
+// })
+
+// FadeTo
+
+// $(document).ready(function() {
+//     $("button").click(function() {
+//         $('div').fadeTo('fast', 0.6);
+//     })
+// })
+
+// SlideDown Те же параметры что и show
+// SlideUp Те же параметры что и hide
+// SlideToggle Те же параметры что и toggle
+
+// Animate
+
+// $(document).ready(function() {
+//     $("button").click(function() {
+//         $('div').animate({
+//             left: "200px",
+//             width: "200px",
+//             height: '50px',
+//             opacity: "0.5"
+//     });
+//     })
+// })  
+
+// Сделайте Header в котором будут разделы. При наведении на раздел Должен появляться выпадающий список с содержимым в два ряда.
+
+// Сделайте toDoList с испольщование пройденных экшенов. 
+
+// За 3 кристаллика. Если пользователь находится на сайте дольше 15 секунд то должно появляться окошко справа снизу*
