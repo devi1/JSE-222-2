bf3a09dd69fa4e1ea3b5ab44baa3d44cc2d1ea8f
Sugar <damiryessenbek@gmail.com>
2023-04-22 12:40:02 +0600

    Project

---
+++ JS/2023.04.22/script.js
@@ -0,0 +1,25 @@
+// let fav = [];
+// let cartTitle = [];
+// let cartDesc = [];
+
+// function addToCart(){
+//     let goods = document.getElementById('goods');
+//     cart.push(goods);
+//     console.log(cart);
+// }
+
+// let a = 9
+// let b = 5
+
+// let sum = (a, b) => a + b
+
+
+// let sum = function(a,b){
+//     return a+b;
+// }
+
+// console.log(sum(a,b));
+
+let sayHi = () => alert("Hi")
+
+sayHi();
